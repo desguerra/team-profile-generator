1e20df48bc4ddd0782a0ba659a321fe0f91ec438
desguerra <ddesguerra@ucdavis.edu>
2022-09-05 15:58:03 -0700

    start making static html dynamic

--- src/page-template.js
+++ src/page-template.js
@@ -1,3 +1,87 @@
+function generateManager() {
+    return `<div class="col-sm-6 col-lg-3 my-2">
+    <div class="card">
+        <div class="card-header">
+            <h2 class="card-title">Manager Name</h2>
+            <h3 class="card-subtitle text-muted">
+                Manager Role
+            </h3>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: 1</li>
+            <li class="list-group-item">
+                Email:
+                <a
+                    href="mailto:manager@fakemail.com"
+                    class="link-info"
+                    target="_blank"
+                    >manager@fakemail.com</a
+                >
+            </li>
+            <li class="list-group-item">Office #: 100</li>
+        </ul>
+    </div>
+</div>`
+};
+
+function generateNextEmployees() {
+    return `<div class="col-sm-6 col-lg-3 my-2">
+    <div class="card">
+        <div class="card-header">
+            <h2 class="card-title">Engineer Name</h2>
+            <h3 class="card-subtitle text-muted">
+                Engineer Role
+            </h3>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: 2</li>
+            <li class="list-group-item">
+                Email:
+                <a
+                    href="mailto:eng@fakemail.com"
+                    class="link-info"
+                    target="_blank"
+                    >eng@fakemail.com</a
+                >
+            </li>
+            <li class="list-group-item">
+                Github:
+                <a
+                    href="https://github.com/octocat"
+                    class="link-info"
+                    target="_blank"
+                    >octocat</a
+                >
+            </li>
+        </ul>
+    </div>
+</div>
+
+<div class="col-sm-6 col-lg-3 my-2">
+    <div class="card">
+        <div class="card-header">
+            <h2 class="card-title">Intern Name</h2>
+            <h3 class="card-subtitle text-muted">
+                Intern Role
+            </h3>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: 14</li>
+            <li class="list-group-item">
+                Email:
+                <a
+                    href="mailto:manager@fakemail.com"
+                    class="link-info"
+                    target="_blank"
+                    >intern@fakemail.com</a
+                >
+            </li>
+            <li class="list-group-item">School: UCD</li>
+        </ul>
+    </div>
+</div>`
+};
+
 function generateTemplate() {
     return `<!DOCTYPE html>
     <html lang="en">
@@ -5,7 +89,7 @@ function generateTemplate() {
             <meta charset="UTF-8" />
             <meta http-equiv="X-UA-Compatible" content="IE=edge" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <title>My Team</title>
+            <title>My Team Profile</title>
     
             <link
                 href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
@@ -17,12 +101,10 @@ function generateTemplate() {
     
         <body>
             <header
-                class="container-fluid text-bg-dark p-3 d-flex justify-content-center"
+                class="container-fluid text-bg-dark p-3 text-center"
             >
-                <h1>My Team!</h1>
-            </header>
-    
-            <section class="container-fluid text-bg-dark p-1 d-flex justify-content-center">
+                <h1 class="py-2">My Team!</h1>
+
                 <p>
                     made with 🤍 by
                     <a
@@ -32,91 +114,17 @@ function generateTemplate() {
                         ><strong>nikki</strong></a
                     >
                 </p>
-            </section>
+            </header>
     
-            <section class="container p-3">
+            <main class="container p-3">
                 <div class="row d-flex justify-content-center">
-                    <div class="col-sm-6 col-lg-3 my-2">
-                        <div class="card">
-                            <div class="card-header">
-                                <h2 class="card-title">Manager Name</h2>
-                                <h3 class="card-subtitle text-muted">
-                                    Manager Role
-                                </h3>
-                            </div>
-                            <ul class="list-group list-group-flush">
-                                <li class="list-group-item">ID: 1</li>
-                                <li class="list-group-item">
-                                    Email:
-                                    <a
-                                        href="mailto:manager@fakemail.com"
-                                        class="link-info"
-                                        target="_blank"
-                                        >manager@fakemail.com</a
-                                    >
-                                </li>
-                                <li class="list-group-item">Office #: 100</li>
-                            </ul>
-                        </div>
-                    </div>
+                    
+                    ${generateManager()}
     
-                    <div class="col-sm-6 col-lg-3 my-2">
-                        <div class="card">
-                            <div class="card-header">
-                                <h2 class="card-title">Engineer Name</h2>
-                                <h3 class="card-subtitle text-muted">
-                                    Engineer Role
-                                </h3>
-                            </div>
-                            <ul class="list-group list-group-flush">
-                                <li class="list-group-item">ID: 2</li>
-                                <li class="list-group-item">
-                                    Email:
-                                    <a
-                                        href="mailto:eng@fakemail.com"
-                                        class="link-info"
-                                        target="_blank"
-                                        >eng@fakemail.com</a
-                                    >
-                                </li>
-                                <li class="list-group-item">
-                                    Github:
-                                    <a
-                                        href="https://github.com/octocat"
-                                        class="link-info"
-                                        target="_blank"
-                                        >octocat</a
-                                    >
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-    
-                    <div class="col-sm-6 col-lg-3 my-2">
-                        <div class="card">
-                            <div class="card-header">
-                                <h2 class="card-title">Intern Name</h2>
-                                <h3 class="card-subtitle text-muted">
-                                    Intern Role
-                                </h3>
-                            </div>
-                            <ul class="list-group list-group-flush">
-                                <li class="list-group-item">ID: 14</li>
-                                <li class="list-group-item">
-                                    Email:
-                                    <a
-                                        href="mailto:manager@fakemail.com"
-                                        class="link-info"
-                                        target="_blank"
-                                        >intern@fakemail.com</a
-                                    >
-                                </li>
-                                <li class="list-group-item">School: UCD</li>
-                            </ul>
-                        </div>
-                    </div>
+                    ${generateNextEmployees()}
+
                 </div>
-            </section>
+            </main>
     
             <script
                 src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
