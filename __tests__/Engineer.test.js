ef335fc8acfb23a27ca281a8548bff093c06f5fa
desguerra <ddesguerra@ucdavis.edu>
2022-09-04 19:56:27 -0700

    add rest of tests for engineer

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -5,7 +5,37 @@ test('creates an engineer object', () => {
 
     expect(engineer.name).toBe('TestName');
     expect(engineer.id).toBe('2');
-    expect(engineer.email).toEqual('testengineer@fakemail.com');
+    expect(engineer.email).toBe('testengineer@fakemail.com');
 
-    expect(engineer.github).toEqual('octocat');
+    expect(engineer.github).toBe('octocat');
+});
+
+test("gets engineer's name", () => {
+    const engineer = new Engineer('TestName', '2', 'testengineer@fakemail.com', 'octocat');
+  
+    expect(engineer.getName()).toEqual('TestName');
+});
+
+test("gets engineer's id", () => {
+    const engineer = new Engineer('TestName', '2', 'testengineer@fakemail.com', 'octocat');
+  
+    expect(engineer.getId()).toEqual('2');
+});
+
+test("gets engineer's email", () => {
+    const engineer = new Engineer('TestName', '2', 'testengineer@fakemail.com', 'octocat');
+  
+    expect(engineer.getEmail()).toEqual('testengineer@fakemail.com');
+});
+
+test("gets engineer's github", () => {
+    const engineer = new Engineer('TestName', '2', 'testengineer@fakemail.com', 'octocat');
+  
+    expect(engineer.getGithub()).toEqual('octocat');
+});
+
+test("gets engineer's role", () => {
+    const engineer = new Engineer('TestName', '2', 'testengineer@fakemail.com', 'octocat');
+  
+    expect(engineer.getRole()).toEqual('Engineer');
 });
